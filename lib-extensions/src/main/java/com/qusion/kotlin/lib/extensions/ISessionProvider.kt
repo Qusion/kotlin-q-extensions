941dd6e6fa7e1fcd81d07fcbb9a3826bb179642a
michona <mitevski1201miki@gmail.com>
2019-10-10 12:11:28 +0200

    Adds session provider

---
+++ lib-extensions/src/main/java/com/qusion/kotlin/lib/extensions/ISessionProvider.kt
@@ -0,0 +1,14 @@
+package com.qusion.kotlin.lib.extensions
+
+/**
+ * Used for session persistance.
+ * Implement using EncryptedSharedPreferences. (ideally)
+ * */
+interface ISessionProvider {
+
+    fun get(): String
+
+    fun set(sid: String)
+
+    fun clear()
+}
